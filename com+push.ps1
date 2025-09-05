3d501698abf5c85d0ebaa358f942c76e05588fbe
stealbe <paradovskiialex@gmail.com>
2025-09-05 12:34:54 +0300

    РђРІС‚РѕРѕР±РЅРѕРІР»РµРЅРёРµ: %date% %time%

---
+++ com+push.ps1
@@ -0,0 +1,23 @@
+# commit-all.ps1
+$commitMessage = "Автообновление: %date% %time%"
+
+# Получаем список worktree и их пути
+$worktrees = git worktree list | ForEach-Object { ($_ -split '\s+')[0] }
+
+foreach ($wt in $worktrees) {
+    Write-Host ">>> Работаю с $wt ..."
+    Set-Location $wt
+
+    git add .
+    $result = git commit -m "$commitMessage" 2>&1
+    if ($LASTEXITCODE -eq 0) {
+        Write-Host "✔ Закоммичено в $wt"
+    } else {
+        Write-Host "ℹ Нет изменений в $wt"
+    }
+
+    Set-Location "E:\Kolledge\2 course"
+}
+
+git push --all origin
+Write-Host ">>> Все ветки запушены!"
